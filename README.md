93bae3c296e8336e9f258588f86339a3305a8de9
Sebastian Arriola <sebdeveloper6952@gmail.com>
2020-11-08 13:45:42 -0600

    task 1.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Miniproyecto6
-Miniproyecto 6 para la clase Modelación y Simulación.
+### How to Run
+1. install Docker
+2. `docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD:/usr/src/myapp" -w /usr/src/myapp rust cargo build --release`
+3. `docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD:/usr/src/myapp" -w /usr/src/myapp rust ./target/release/genetic-algorithms`
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+mod tsp;
+mod functions_2d;
+mod functions_2d_2;
+mod functions_2d_3;
+
+fn main() {
+
+    // Task 1
+    let mut pop = Vec::new();
+
+    // Crear poblacion inicial con 100 individuos.
+    for _i in 0..100 {
+        pop.push(functions_2d::Individual2D::new());
+    }
+    
+    // Se ordena la poblacion de acuerdo al fitness.
+    pop.sort_by(|a, b| a.fitness().partial_cmp(&b.fitness()).unwrap());
+
+    // correr 1 millon de iteraciones
+    for _i in 0..1000000 {
+        
+        // cada iteracion se crean 10 nuevos individuos y se "matan"
+        // a los peores 10.
+        for i in 0..10 {
+            let new = pop[i].offspring(&pop[i + 1]);
+            pop.push(new);
+        }
+
+        pop.sort_by(|a, b| a.fitness().partial_cmp(&b.fitness()).unwrap());
+
+        // "matar" a los peores 10.
+        for _i in 0..10 {
+            pop.remove(0);
+        }
+    }
+
+    // el mejor individuo esta al frente de la lista.
+    println!("\nTask 1 Best individual:");
+    println!("{:?}", pop[pop.len() - 1]);
+}
